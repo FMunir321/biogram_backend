504f6f28938c4b1b8b5162c0a1b97746469e313a
fmunir321 <faisalmunir.medsnexus@gmail.com>
2025-06-30 19:41:17 +0500

    protect the social link with auth middlerware

--- routes/socialLinkRoutes.js
+++ routes/socialLinkRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/socialLinkController');
+const auth = require('../middleware/auth');
 
-router.post('/', controller.createSocialLink);
-router.delete('/:id', controller.deleteSocialLink);
-router.get('/:userId', controller.getUserSocialLinks);
+router.post('/', auth, controller.createSocialLink);
+router.delete('/:id', auth, controller.deleteSocialLink);
+router.get('/:userId', auth, controller.getUserSocialLinks);
 
 module.exports = router;
