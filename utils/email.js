2b8bf3cfcfa020bfc78e296129fc0c844b3b3ade
fmunir321 <faisalmunir.medsnexus@gmail.com>
2025-07-01 10:53:11 +0500

    delete extra files

--- utils/email.js
+++
@@ -1,9 +0,0 @@
-const nodemailer = require('nodemailer');
-
-module.exports = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: process.env.AUTH_EMAIL,
-    pass: process.env.AUTH_PASS
-  }
-});
